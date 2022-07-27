e6af8d7eecbb56e9d44362c3ccc0a1638674cdeb
Nicholas Cellino <nacellin@buffalo.edu>
2022-07-27 00:33:47 -0400

    start prototyping how to register interpreters

--- src/browser_eval/main.cljs
+++ src/browser_eval/main.cljs
@@ -29,7 +29,27 @@
       (.getElementById "eval-btn")
       (.addEventListener "click" eval-user-input)))
 
+(.log js/console (-> js/document
+                     (.querySelectorAll "#resul")))
+
+(.-innerHTML (aget (.querySelectorAll js/document "#result") 0))
+(.-value (aget (.querySelectorAll js/document "[clj-interpreter] [clj-code]") 0))
+
+; adding event listener to thing i queried for
+(.addEventListener (aget (.querySelectorAll js/document "[clj-interpreter] [clj-code]") 0) "click" #(.log js/console "clicked!"))
+
+(.-value (aget (.querySelectorAll js/document "#code") 0))
+
 (comment
   (register-eval-listener)
 
   (replace-elem "result" "ff"))
+
+; Find all clj-interpreters.
+; attach the eval-user-input thingy to the element
+(let [interpreters (.querySelectorAll js/document "[clj-interpreter]")]
+  (map #(.log js/console %) interpreters)
+  (map
+    (fn [i]
+      (.log js/console (aget (.querySelectorAll i "[clj-code]") 0)))
+    interpreters))
